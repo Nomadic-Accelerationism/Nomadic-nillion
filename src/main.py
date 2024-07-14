5a2bb04d7436c357f98a7cbdaec4fa241eac337c
Erik Valle <erik.valle@gmail.com>
2024-07-14 06:40:19 +0200

    add endpoint to compare filters
    add endpoint to compare filters

--- src/main.py
+++ src/main.py
@@ -1,12 +1,29 @@
+from flask import Flask, jsonify, request
+import requests 
+import json
 from nada_dsl import *
+app = Flask(__name__)
 
-def nada_main():
-    party1 = Party(name="Party1")
-    party2 = Party(name="Party2")
-    party3 = Party(name="Party3")
+
+@app.route("/")
+def helloworld():
+    return 'N/ACC'
+    
+
+
+@app.route('/compare', methods=['GET'])
+def compare():
+    interestFrom = request.args.get('interestFrom')
+    interinterestToest2 = request.args.get('interestTo')
+    party1 = Party(name="interestFrom")
+    party2 = Party(name="interestTo")
     a = SecretInteger(Input(name="A", party=party1))
     b = SecretInteger(Input(name="B", party=party2))
 
-    result = a + b
+    resultFrom = a > 1
+    resultTo = b > 1
+    result = resultFrom + resultTo
 
     return [Output(result, "my_output", party3)]
+
+
